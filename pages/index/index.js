e706107d062aca9173af94b4db1cdedb4ca15554
tang <2684270465@qq.com>
2019-12-11 14:02:31 +0800

    内置组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,25 @@
 Page({
   data: {
-    count: 0
+    count: 0,
+    image:null
   },
   handlerClick() {
     this.setData({
       count: this.data.count + 1
     });
+  },
+  handlerImgLoad(){
+    console.log("image load...");
+  },
+  chooseImage(){
+    wx.chooseImage({
+      success: (res) => {
+        
+        this.setData({
+          image: res.tempFilePaths[0]
+        });
+        
+      },
+    })
   }
 });
