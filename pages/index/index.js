c306d91e8c9a66e2b5e124d47947c62dce4023cb
tang <2684270465@qq.com>
2019-12-11 09:25:18 +0800

    first

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,5 @@
 Page({
   data: {
-    students: [{
-        name: "tang01",
-        age: 18
-      },
-      {
-        name: "tang02",
-        age: 15
-      },
-      {
-        name: "tang03",
-        age: 10
-      },
-    ],
     count: 0
   },
   handlerClick() {
